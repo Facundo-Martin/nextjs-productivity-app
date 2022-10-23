664a81e8707e7d5b229b98d965e1f64273893b16
Facundo-Martin <facundomartin98@gmail.com>
2022-10-23 16:08:31 -0700

    styled header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,15 +1,32 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between">
+    <header className="flex items-center justify-between header">
       <h1>Header</h1>
       <nav>
         <ul className="flex items-center gap-x-4">
-          <li className="nav-item">Home</li>
-          <li className="nav-item">Goals</li>
-          <li className="nav-item">Tools</li>
-          <li className="nav-item">Home</li>
+          <li>
+            <Link href="/">
+              <a className="nav-item">Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/goals">
+              <a className="nav-item">Goals</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/tools">
+              <a className="nav-item">Tools</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/other">
+              <a className="nav-item">Other</a>
+            </Link>
+          </li>
         </ul>
       </nav>
       <h1>Header</h1>
