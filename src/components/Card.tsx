0a16f7726eccdceec80fc89914c25c55b14348a5
Facundo-Martin <facundomartin98@gmail.com>
2022-10-23 16:24:32 -0700

    added Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+import React from "react";
+
+function Card({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={clsx(
+        className,
+        "rounded-2xl border border-zinc-200 p-6 dark:border-zinc-600/70 shadow-sm dark:shadow-neutral-700/60 hover:shadow-md"
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Card;
--- src/components/Goals/GoalsCard.tsx
+++ src/components/Goals/GoalsCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import { weeklyGoal } from "../../interfaces";
 
-export default function GoalsCard() {
-  return <div>GoalsCard</div>;
+export default function GoalsCard({ goals }: { goals: weeklyGoal }) {
+  return <Card>GoalsCard</Card>;
 }
