c1b9bea9250bd5751f83b9bb9564116a693a8a1b
Facundo-Martin <facundomartin98@gmail.com>
2022-10-23 16:02:30 -0700

    created dummy header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Header() {
-  return <div className="">Header</div>;
+  return (
+    <header className="flex items-center justify-between">
+      <h1>Header</h1>
+      <nav>
+        <ul className="flex items-center gap-x-4">
+          <li className="nav-item">Home</li>
+          <li className="nav-item">Goals</li>
+          <li className="nav-item">Tools</li>
+          <li className="nav-item">Home</li>
+        </ul>
+      </nav>
+      <h1>Header</h1>
+    </header>
+  );
 }
